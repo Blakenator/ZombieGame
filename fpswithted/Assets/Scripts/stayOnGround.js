87b42b3791faea8f3bdc2aa9555d1e30aa71f715
Keegan Burr <Burr.k@stu.janesville.k12.wi.us>
2013-07-02 09:58:54 -0500

    More AI improvements

--- fpswithted/Assets/Scripts/stayOnGround.js
+++ fpswithted/Assets/Scripts/stayOnGround.js
@@ -1,12 +1,12 @@
 #pragma strict
-
+var parent:GameObject;
 function Start () {
-
+	parent=transform.parent.gameObject;
+	Debug.Log(parent);
 }
-
 function Update () {
 		var hit : RaycastHit;
-        if (Physics.Raycast (transform.position, -Vector3.up, hit, 100.0)) {
-            transform.position.y=hit.transform.position.y+renderer.bounds.size.y/2;
+        if (Physics.Raycast (transform.position, -Vector3.up, hit, 1000.0)) {
+            transform.position.y=hit.transform.position.y+ parent.renderer.bounds.size.y/2;
         }
 }
