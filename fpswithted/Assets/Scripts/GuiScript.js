bc823825bfc80bad8fb7c6677670238579fac4f0
Keegan Burr <Burr.k@stu.janesville.k12.wi.us>
2013-07-07 14:40:31 -0500

    Start of an updated main menu

--- fpswithted/Assets/Scripts/GuiScript.js
+++ fpswithted/Assets/Scripts/GuiScript.js
@@ -1,11 +1,26 @@
 #pragma strict
+private var optionsCam:Camera;
+function Start(){
+	optionsCam=gameObject.Find("Options Camera").GetComponent(Camera);
+}
 
 function OnGUI(){
-	if(GUI.Button(Rect(Screen.width/2-115,Screen.height/2+150,230,40),"Let's Go Kill Some Zombies!")){
+	if(GUI.Button(Rect(Screen.width/2-115,Screen.height/2+100,230,40),"Let's Go Kill Some Zombies!")){
 		Application.LoadLevel(1);
 	}
+	
+	if(GUI.Button(Rect(Screen.width/2-87,Screen.height/2+150,175,40),"Options")){
+		Debug.Log("will swap!");
+		swapMenu(this.GetComponent(Camera),optionsCam);
+	}
+	
 	if(GUI.Button(Rect(Screen.width/2-60,Screen.height/2+200,120,40),"Quit!")){
 		Application.Quit();
 	}
 }
 
+function swapMenu(From:Camera,To:Camera){
+		From.enabled=false;
+		To.enabled=true;
+		Debug.Log("SWAP!");
+}
