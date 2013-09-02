6e8f45edb9b271f517b489fada87e4196883fb8e
Blake Stacks <blakestacks@rocketmail.com>
2013-09-02 18:04:53 -0500

    Optimizer

---
+++ fpswithted/Assets/Scripts/SpawnOptimizer.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var spawns:GameObject[];
+var visiblerange:double;
+function Start () {
+	spawns=GameObject.FindGameObjectsWithTag("ObjSpawn");
+}
+var cntr:int;
+function Update () {
+	cntr=(cntr>spawns.Length)?0:cntr++;
+	try{
+		var tmp:boolean=(!spawns[cntr].renderer.isVisible||Vector3.Distance(GameObject.Find("player").transform.position,transform.position)>visiblerange)?false:true;
+		spawns[cntr].active=tmp;
+	}catch(e){
+	
+	}
+}
